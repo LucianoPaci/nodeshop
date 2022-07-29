8d6e1bfec0e7523d5eaf636dab176386da96e9d4
Luciano Paci <lucianopaci93@gmail.com>
2022-07-29 15:44:00 -0300

    Removed Logo

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,10 @@
 import { useRoutes } from 'react-router-dom'
-import { ReactComponent as Logo } from './logo.svg'
 import routes from './app/routes'
 import './App.css'
 
 function App() {
   const renderedRoutes = useRoutes(routes)
-  return (
-    <div className="App">
-      {renderedRoutes}
-      <header className="App-header">
-        <Logo className="App-logo" />
-      </header>
-    </div>
-  )
+  return <div className="App">{renderedRoutes}</div>
 }
 
 export default App
